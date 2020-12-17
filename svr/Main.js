e5673339d2eb9928dae85c27913df9200f06542f
chenhaipei <2362585075@qq.com>
2020-12-17 22:13:56 +0800

    link login user in database.

--- svr/Main.js
+++ svr/Main.js
@@ -12,78 +12,126 @@ let onlineUserMap = new zTool.SimpleMap();
 let historyContent = new zTool.CircleList(100);
 let connCounter = 1;
 let uid = null;
+let mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/chatroom')
+let db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error:'));
+db.once('open',function(){
+    console.log('db connection is OK');
+});
+
+let loginSchema = mongoose.Schema({
+    uid: Number,
+    nickName: String,
+    loginDate: Date
+})
+
+let logoutSchema = mongoose.Schema({
+    uid: Number,
+    nickName: String,
+    loginDate: Date
+})
+
+let loginUserModel = mongoose.model("loginUser",loginSchema);
+let logouUserModel = mongoose.model("logoutUser",logoutSchema);
 
 wss.on('connection', function (conn) {
     conn.on('message', function (message) {
         let mData = chatLib.analyzeMessageData(message);
 
-        if (mData && mData.EVENT) {
-            switch (mData.EVENT) {
-                case EVENT_TYPE.LOGIN:
-                    // New user connection
-                    uid = connCounter;
-                    let newUser = {
-                        'uid': connCounter,
-                        'nick': chatLib.getMsgFirstDataValue(mData)
-                    };
-                    console.log('User:{\'uid\':' + newUser.uid + ',\'nickname\':' + newUser.nick + '}coming on protocol websocket draft ' + conn.protocolVersion);
-                    console.log('current connecting counter: ' + wss.clients.length);
-                    console.log(uid);
-                    // Add newly connected users to the list of online users
-                    onlineUserMap.put(uid, newUser);
-                    console.log(onlineUserMap);
-                    connCounter++;
-                    // Broadcast information about new users to online users
-                    wss.clients.forEach(function (client) {
-                        client.send(JSON.stringify({
+        if (mData) {
+            if (mData.EVENT) {
+                switch (mData.EVENT) {
+                    case EVENT_TYPE.LOGIN:
+                        // New user connection
+                        uid = connCounter;
+                        let newUser = {
+                            'uid': connCounter,
+                            'nick': chatLib.getMsgFirstDataValue(mData)
+                        };
+                        console.log('User:{\'uid\':' + newUser.uid + ',\'nickname\':' + newUser.nick + '}coming on protocol websocket draft ' + conn.protocolVersion);
+                        console.log('current connecting counter: ' + wss.clients.length);
+                        console.log(uid);
+                        // Add newly connected users to the list of online users
+                        onlineUserMap.put(uid, newUser);
+
+                        //db
+                        let user = new loginUserModel({uid: newUser.uid, nickName: newUser.nick, loginDate: Date.now()})
+                        user.save(function (err, loginUser) {
+                            if (err) return console.log(err);
+                            console.log("data save");
+                        });
+
+                        console.log(onlineUserMap);
+                        connCounter++;
+                        // Broadcast information about new users to online users
+                        wss.clients.forEach(function (client) {
+                            client.send(JSON.stringify({
+                                'user': onlineUserMap.get(uid),
+                                'event': EVENT_TYPE.LOGIN,
+                                'values': [newUser],
+                                'counter': connCounter
+                            }))
+                        });
+                        break;
+
+                    case EVENT_TYPE.SPEAK:
+                        // The user speaks something
+                        let content = chatLib.getMsgSecondDataValue(mData);
+                        //Synchronize user message
+                        wss.clients.forEach(function (client) {
+                            client.send(JSON.stringify({
+                                'user': onlineUserMap.get(chatLib.getMsgFirstDataValue(mData)),
+                                'event': EVENT_TYPE.SPEAK,
+                                'values': [content]
+                            }))
+                        });
+                        // for (let j = 0; j < wss.clients.length; j++) {
+                        //     wss.clients[j].send(JSON.stringify({
+                        //         'user': onlineUserMap.get(chatLib.getMsgFirstDataValue(mData)),
+                        //         'event': EVENT_TYPE.SPEAK,
+                        //         'values': [content]
+                        //     }));
+                        // }
+                        historyContent.add({
                             'user': onlineUserMap.get(uid),
-                            'event': EVENT_TYPE.LOGIN,
-                            'values': [newUser],
-                            'counter': connCounter
-                        }))
-                    });
-                    break;
+                            'content': content,
+                            'time': new Date().getTime()
+                        });
+                        break;
 
-                case EVENT_TYPE.SPEAK:
-                    // The user speaks something
-                    let content = chatLib.getMsgSecondDataValue(mData);
-                    //Synchronize user message
-                    for (let j = 0; j < wss.clients.length; j++) {
-                        wss.clients[j].send(JSON.stringify({
-                            'user': onlineUserMap.get(chatLib.getMsgFirstDataValue(mData)),
-                            'event': EVENT_TYPE.SPEAK,
-                            'values': [content]
+                    case EVENT_TYPE.LIST_USER:
+                        // Get the current online user
+                        conn.send(JSON.stringify({
+                            'user': onlineUserMap.get(uid),
+                            'event': EVENT_TYPE.LIST_USER,
+                            'values': onlineUserMap.values()
                         }));
-                    }
-                    historyContent.add({
-                        'user': onlineUserMap.get(uid),
-                        'content': content,
-                        'time': new Date().getTime()
-                    });
-                    break;
+                        break;
 
-                case EVENT_TYPE.LIST_USER:
-                    // Get the current online user
-                    conn.send(JSON.stringify({
-                        'user': onlineUserMap.get(uid),
-                        'event': EVENT_TYPE.LIST_USER,
-                        'values': onlineUserMap.values()
-                    }));
-                    break;
+                    case EVENT_TYPE.LIST_HISTORY:
+                        // Get the most recent chat history message
+                        conn.send(JSON.stringify({
+                            'user': onlineUserMap.get(uid),
+                            'event': EVENT_TYPE.LIST_HISTORY,
+                            'values': historyContent.values()
+                        }));
+                        break;
 
-                case EVENT_TYPE.LIST_HISTORY:
-                    // Get the most recent chat history message
-                    conn.send(JSON.stringify({
-                        'user': onlineUserMap.get(uid),
-                        'event': EVENT_TYPE.LIST_HISTORY,
-                        'values': historyContent.values()
-                    }));
-                    break;
+                    default:
+                        break;
+                }
 
-                default:
-                    break;
+            } else {
+                // There was an error with the event type.
+                // Load and log,
+                // Send an error message to the current user
+                console.log('desc:message,userId:' + chatLib.getMsgFirstDataValue(mData) + ',message:' + message);
+                conn.send(JSON.stringify({
+                    'uid': chatLib.getMsgFirstDataValue(mData),
+                    'event': EVENT_TYPE.ERROR
+                }));
             }
-
         } else {
             // There was an error with the event type.
             // Load and log,
